d6f9984ce776e90ca245ed85305d838adfc0dc64
unknown <02230116.cst@rub.edu.bt>
2024-02-26 15:21:21 +0600

    discunt eligibility

--- discount_eligibility.py
+++ discount_eligibility.py
@@ -1,8 +1,13 @@
-age = int(input("Your Age: "))
-student = input("Are You Student?(Yes/No): ")
 
-if (age >= 12 and (age >= 13 or age <= 18 and student == "yes")):
-    print("You are eigible for the discount")
+def eligibility_test():
 
-else:
-    print("Sorry,you are not eligible for the discount")
+    age = int(input("Your Age: "))
+    student = input("Are You Student?(Yes/No): ")
+
+    if (age <= 12 or ((age >= 13 or age <= 18) and student == "yes")):
+        print("You are eigible for the discount on the movie ticket")
+
+    else:
+        print("Sorry, you are not eligible for the discount")
+
+eligibility_test()
