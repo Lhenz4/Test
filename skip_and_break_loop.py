310157c485229273c96d45660648ba241f794d06
unknown <02230116.cst@rub.edu.bt>
2024-03-18 15:42:04 +0600

    skip at 3 and break the loop at 7

--- skip_and_break_loop.py
+++ skip_and_break_loop.py
@@ -3,6 +3,6 @@ for i in range (1,10):
         print(f"skipping {i} in the inner loop")
         continue
     elif i == 7:
-        print(f"Reached {i}, breaking outer loop")
+        print(f"Reached {i}, outer loop broken")
         break
     print(i)
